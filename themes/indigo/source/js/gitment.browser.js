80fda422f79377c51d6f6f4a1593260c8199b7bf
yuangezhizao <root@yuangezhizao.cn>
2018-11-15 23:10:33 +0800

    修复评论系统

--- themes/indigo/source/js/gitment.browser.js
+++ themes/indigo/source/js/gitment.browser.js
@@ -3809,7 +3809,7 @@ var Gitment =
                     }, options);
 
                     this.state.user.isLoggingIn = true;
-                    _utils.http.post('https://gh-oauth.imsun.net', {
+                    _utils.http.post('https://auth.baixiaotu.cc', {
                         code: code,
                         client_id: client_id,
                         client_secret: client_secret
