8fe9beadad005b8f0834693b7a732f51b7ecbaf3
yuangezhizao <root@yuangezhizao.cn>
2019-01-01 13:54:42 +0800

    更新部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
 set -ev
 
-# get clone master
-git clone "https://${TravisCIToken}@${GH_REF}" .deploy_git
-cd .deploy_git
-git checkout master
+git clone "https://yuangezhizao:${CodingToken}@${CD_REF}" coding_public
 
-cd ../
-mv .deploy_git/.git/ ./public/
+mv ./coding_public/.git/ ./public/
 
 cd ./public
 
 git config user.name "yuangezhizao"
 git config user.email "root@yuangezhizao.cn"
 
-# add commit timestamp
 git add .
 git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M:%S"`"
 
-# Github Pages
-#git push --force --quiet "https://${TravisCIToken}@${GH_REF}" master:master
-
-# Coding Pages
 git push --force --quiet "https://yuangezhizao:${CodingToken}@${CD_REF}" master:master
