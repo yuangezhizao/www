2dd4172e8a4b23eb71307d86410e9268dc7c4ef1
yuangezhizao <root@yuangezhizao.cn>
2019-08-25 22:42:13 +0800

    Coding 个人版 → 企业版

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 
-git clone "https://yuangezhizao:${CodingToken}@${CD_REF}" coding_public
+git clone "https://${CodingUser}:${CodingToken}@${CodingRepo}" coding_public
 
 mv ./coding_public/.git/ ./public/
 
@@ -13,4 +13,4 @@ git config user.email "root@yuangezhizao.cn"
 git add .
 git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M:%S"`"
 
-git push --force --quiet "https://yuangezhizao:${CodingToken}@${CD_REF}" master:master
+git push --force --quiet "https://${CodingUser}:${CodingToken}@${CodingRepo}" master:master
