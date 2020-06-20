dec4d65c5e11f498a323ab5e3816ca082acf702e
yuangezhizao-workaccount <chenyuan.gao@alsi.cn>
2020-06-20 13:37:02 +0800

    Fix

--- api/index.py
+++ api/index.py
@@ -1,14 +1,13 @@
-from flask import Response, jsonify
+import time
 
-from . import app
+from flask import Flask, jsonify, g, request, Response
+
+app = Flask(__name__)
 
 
 @app.before_request
 def before_request():
     g.start_time = time.time()
-    if 'profile' in request.args:
-        g.profiler = Profiler()
-        g.profiler.start()
 
 
 @app.after_request
@@ -17,19 +16,9 @@ def after_request(response):
         response.headers.add('LAB-Uncompressed-Content-Length', response.headers['Content-Length'])
     start_to_stop_time = time.time() - g.start_time
     response.headers.add('LAB-Response-Time', start_to_stop_time)
-    if not hasattr(g, 'profile'):
-        return response
-    g.profiler.stop()
-    output_html = g.profiler.output_html()
-    return make_response(output_html)
 
 
-@app.route('/')
-def index():
-    return 'index'
-
-
-@app.route('/ip')
+@app.route('/api/ip')
 def get_my_ip():
     return jsonify({
         'IP': request.remote_addr,
@@ -39,3 +28,9 @@ def get_my_ip():
         'X-Originating-IP': request.headers.get('X-Originating-IP', None),
         'X-Forwarded-For': request.headers.get('X-Forwarded-For', None),
     })
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
